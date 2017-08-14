4efd2346e6ec332746059940fa09161a2974b2dd
vaishnaviyadala <30976906+vaishnaviyadala@users.noreply.github.com>
2017-08-14 07:45:19 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,20 @@
 //counter code
-var button = document.getElementById("counter")
-var counter=0;
+var button = document.getElementById("counter");
 button.onclick=function (){
   counter=counter+1;
-  var span = document.getElementById("count")
-  span.innerHTML=counter.toString();
+   var request = new XHTMLhttpRequest();
+   
+   request.onreadystatechange =function (){
+    if(request.readystate==XHTMLhttpRequest.DONE){
+         if(request.status==200) {
+             var counter=request.responceText;
+             var span = document.getElementById("count");
+             span.innerHTML = counter.toString();
+         }
+    }
+};
+
+requset.open('GET','http://vaishuyadala1997.imad.hasura-app.io/counter',true);
+request.send(null);
+
 };
